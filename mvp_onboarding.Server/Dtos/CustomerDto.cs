cbd1c5f4b2f24310b213e6191dcd6a419ce58e7f
Andres Marin <andres.marin@gmx.com>
2024-10-01 11:46:24 +1000

    Update Dtos to reflect latest db annotations

--- mvp_onboarding.Server/Dtos/CustomerDto.cs
+++ mvp_onboarding.Server/Dtos/CustomerDto.cs
@@ -1,5 +1,4 @@
-﻿using mvp_onboarding.Server.Models;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace mvp_onboarding.Server.Dtos
 {
@@ -8,11 +7,11 @@ namespace mvp_onboarding.Server.Dtos
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Customer name is required.")]
-        [StringLength(100, MinimumLength = 6)]
+        [StringLength(100, MinimumLength = 1)]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "Customer address is required.")]
-        [StringLength(500, MinimumLength = 6)]
+        [StringLength(500, MinimumLength = 1)]
         public string? Address { get; set; }
     }
 }
--- mvp_onboarding.Server/Dtos/CustomerUpdateDto.cs
+++ mvp_onboarding.Server/Dtos/CustomerUpdateDto.cs
@@ -1,20 +1,19 @@
-﻿using mvp_onboarding.Server.Models;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace mvp_onboarding.Server.Dtos
 {
     public class CustomerUpdateDto
     {
-        [Required] 
+        [Required]
         [Range(1, int.MaxValue, ErrorMessage = "Id must be a positive integer.")]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Customer name is required.")]
-        [StringLength(100, MinimumLength = 6)]
+        [StringLength(100, MinimumLength = 1)]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "Customer address is required.")]
-        [StringLength(500, MinimumLength = 6)]
+        [StringLength(500, MinimumLength = 1)]
         public string? Address { get; set; }
     }
 }
