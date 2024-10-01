27d8beb940e24c4ad944f4b48d9ce58e56991cf3
Andres Marin <andres.marin@gmx.com>
2024-10-01 16:24:28 +1000

    Adding API support for SalesView for easier sales listing

---
+++ mvp_onboarding.Server/Interfaces/ISalesViewMethods.cs
@@ -0,0 +1,9 @@
+﻿using mvp_onboarding.Server.Dtos;
+
+namespace mvp_onboarding.Server.Interfaces
+{
+    public interface ISalesViewMethods
+    {
+        Task<IEnumerable<SalesViewDto>> GetSales();
+    }
+}
