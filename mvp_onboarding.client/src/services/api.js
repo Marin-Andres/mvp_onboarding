c0c434809837b955c512680a91db9c422c51fcee
Andres Marin <andres.marin@gmx.com>
2024-10-04 20:48:08 +1000

    Adjusting base URL for deployment

--- mvp_onboarding.client/src/services/api.js
+++ mvp_onboarding.client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:5173/api', 
+  //baseURL: 'https://localhost:5173/api', 
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://talentonboarding-fhaeg9hvafb6gwfs.australiaeast-01.azurewebsites.net/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
